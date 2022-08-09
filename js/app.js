dae27e353c138e2fd3b8c028353fe2fa1af65066
OnlyDropped <asheburenkov@yandex.ru>
2022-08-10 00:53:58 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$('.main-slider').slick({
+  useTransform: true,
+  //cssEase: 'ease-out',
+  cssEase: 'cubic-bezier(0.600, -0.280, 0.735, 0.045)',
+  prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/arrow-prev.svg" alt=""></button>',
+  nextArrow: '<button type="button" class="slick-next"><img src="img/icons/arrow-next.svg" alt=""></button>'
+});
